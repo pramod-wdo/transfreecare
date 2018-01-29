d3ee61edc8178c500304e762911dfc23e0d33be6
pramod-wdo <pramodhc007@gmail.com>
2018-01-29 20:25:56 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Transfreecare::Application.config.secret_key_base = '2d7f15df87d0ca67f4791967e0a1caddc6f7064c5720d5a75831239f21d48685077a8e788d81f270921415b02889dc1119fe3d13d5914307f79fafbb7f26e2fd'
