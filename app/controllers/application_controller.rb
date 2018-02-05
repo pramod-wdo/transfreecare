4f2eb3aca108d45f0e8477d8e73475e67da22c55
Pramod <pramodhc007@gmail.com>
2018-02-05 18:18:54 +0530

    Adds static page (#1)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-  	render_html: "Hello World"
+  	render html: "Hello World"
   end	
 end
